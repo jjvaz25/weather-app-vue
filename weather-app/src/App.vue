1274f85b2e91bf58485586f6d2e600097cc8c77c
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-01-05 18:19:08 -0500

    add search bar with console.log of important data

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <top-nav />
     <v-content>
-
+      <weather />
     </v-content>
     <bottom-footer />
   </v-app>
@@ -11,13 +11,15 @@
 <script>
 import TopNav from './components/TopNav'
 import BottomFooter from './components/BottomFooter'
+import Weather from './components/Weather'
 
 export default {
   name: 'App',
 
   components: {
     'top-nav': TopNav,
-    'bottom-footer': BottomFooter
+    'bottom-footer': BottomFooter,
+    'weather': Weather
   },
 
   data: () => ({
---
+++ weather-app/src/components/Weather.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="weather">
+    <v-container style="max-width: 300px">
+      <v-form align="center">
+        <v-text-field 
+          class="v-input input"
+          v-model="zipcode" 
+          label="Enter a zipcode"
+        >
+        </v-text-field>
+        <v-btn class="primary mt-2" @click="getWeather">
+          search
+        </v-btn>
+      </v-form>
+    </v-container>
+
+    <v-container>
+      <v-card
+        class="mx-auto"
+        max-width="400"
+        outlined
+      >
+      <v-row class="pink" align="center">
+        
+        <v-list-item>
+          <v-col cols="8">
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1 text-wrap">{{ this.weatherInfo.name }}</v-list-item-title>
+              <v-list-item-subtitle>Insert date here</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-col>
+          
+          <v-col cols="4" class="green" align="center">
+            <v-list-item-avatar
+              tile
+              size="80"
+              color="primary"
+              class="mx-auto"
+            >
+            </v-list-item-avatar>
+            <v-list-item-subtitle>{{ this.weatherInfo.description }}</v-list-item-subtitle>
+          </v-col>
+          
+
+        </v-list-item>
+        </v-row>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Weather',
+
+  data() {
+    return {
+      zipcode: '',
+      weatherInfo: {}
+    }
+  },
+
+  methods: {
+    async getWeather() {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${this.zipcode},us&APPID=115cf28cc50f49d3ab9fd208f9487bc4`, {mode: 'cors'})
+      const data = await response.json()
+      console.log(data)
+      let weatherObject = {
+        main: data['weather'][0]['main'],
+        description: data['weather'][0]['description'],
+        temp: data['main']['temp'],
+        feelsLike: data['main']['feels_like'],
+        minTemp: data['main']['temp_min'],
+        maxTemp: data['main']['temp_max'],
+        humidity: data['main']['humidity'],
+        wind: data['wind']['speed'],
+        country: data['sys']['country'],
+        sunrise: new Date(data['sys']['sunrise']),
+        sunset: new Date(data['sys']['sunset']),
+        name: data['name']
+      }
+      this.weatherInfo = weatherObject
+      console.log(this.weatherInfo)
+      this.zipcode = ''
+    }
+  }
+}
+
+</script>
+
+<style>
+  .theme--light.v-input:not(.v-input--is-disabled) input {
+    text-align: center
+  }
+</style>

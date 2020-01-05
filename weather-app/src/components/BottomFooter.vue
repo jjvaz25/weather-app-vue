c91e7092e2533f2705c84946d683211fcdeb361c
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-01-05 11:44:18 -0500

    add top nav and bottom footer

---
+++ weather-app/src/components/BottomFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer
+    app
+    flat
+    color="primary"
+  >
+    <span class="white--text">&copy; 2019</span>
+  </v-footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+
+}
+</script>
---
+++ weather-app/src/components/TopNav.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav>
+    <v-app-bar
+      app
+      flat
+      color="primary"
+      dark
+    >
+      <v-toolbar
+        flat
+        color="primary" 
+      >
+        <v-toolbar-title>          
+            <span>Spartan</span>
+            <span class="font-weight-medium">Weather</span>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        
+        <div class="text-center">
+          <v-menu
+            offset-y
+            origin="top center"
+            transition="scale-transition"
+          >
+            <template 
+              v-slot:activator="{ on }"
+            >
+              <v-btn 
+                v-on="on"
+                text
+              >
+                <v-icon>person</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-icon small left>person</v-icon>
+                <v-list-item-title>My Account</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-icon small left>exit_to_app</v-icon>
+                <v-list-item-title>Sign out</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+
+      </v-toolbar>
+    </v-app-bar>
+
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'TopNav',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
